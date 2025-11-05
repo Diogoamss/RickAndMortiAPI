b30b7d347cf46ee6188f21829f1b26c0da39beb7
diogoamss <diogoamss@gmail.com>
2025-11-05 15:16:12 -0300

    w.i.p | 0.0.5 iniciando

---
+++ public/Screens/ListScreen.js
@@ -0,0 +1,8 @@
+import { view, FlatList, Text, Image, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
+import { styles } from '../Styles/Styles.js';
+import React from 'react';
+
+
+
+
