ca79473625e6f362b36364fc64da4b7d88ab9525
diogoamss <diogoamss@gmail.com>
2025-11-05 15:49:23 -0300

    w.i.p | 0.0.6 ListScreen + CharacterList feitos

--- public/Components/CharacterList.js
+++ public/Components/CharacterList.js
@@ -1,3 +1,48 @@
-import { view } from 'react-native';
+import React, { useEffect, useState } from 'react ';
+import { FlatList, Text, Image, ActivityIndicator, TouchableOpacity, view } from 'react-native';
+import { styles } from '../Styles/Styles.js';
+import { ActivityIndicator } from 'react-native-web';
 
+const CharacterList = () => {
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const loadCharacters = async () => {
+            try { 
+                const data = await fetchCharacters();
+                setCharacters(data.results);
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+        loadCharacters();
+    }, []);
+
+    if(loading) {
+         return <ActivityIndicator size="large" color="#0000ff" />;
+    }
+
+    if(error) {
+        return <Text>Error: {error}</Text>;
+    }
+
+    return (
+        <FlatList
+        data={characters}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({item}) => (
+            <View style={styles.characterItem}>
+                <Text style={styles.characterName}>{item.name}</Text>
+                <image source={{url: item.image}} style={styles.characterImage} />
+            </View>
+        )}
+        />
+    );
+};
+
+export default CharacterList;
 
--- public/Screens/ListScreen.js
+++ public/Screens/ListScreen.js
@@ -1,7 +1,55 @@
-import { view, FlatList, Text, Image, TouchableOpacity } from 'react-native';
-import { useEffect, useState } from 'react';
+import { view, FlatList, ActivityIndicator, Text, Image, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { styles } from '../Styles/Styles.js';
-import React from 'react';
+import CharacterList from '../Components/CharacterList.js';
+import { fetchCharacters } from '../api/rickAndMorty';
+
+
+const ListScreen = () => {
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() =>{
+        const LoadCharacters = async () => {
+            try{ 
+                const data = await fetchCharacters();
+                setCharacters(data.results);
+            } catch (err) {
+                setError(err.message);
+            } finally{
+                setLoading(false);
+            }
+        };
+        LoadCharacters();
+    }, []);
+
+    if (Loading){
+        return (
+            <View style={styles.loadingContainer}>
+                <ActivityIndicator size="Large" color="#0000ff" />
+            </View>
+        );
+    }
+
+    if (error) {
+        return (
+            <View style={styles.errorContainer}>
+                <Text>Error: {error}</Text>
+            </View>
+        );
+    }
+
+    return (
+        <FlatList
+        data={characters}
+        renderItem={({item}) => <CharacterList character={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        />
+    );
+};
+
+export default ListScreen;
 
 
 
